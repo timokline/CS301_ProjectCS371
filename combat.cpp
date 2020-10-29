61ff8e84ce98dfe690de263a2f640c7445e83315
timothyakline <44985180+timothyakline@users.noreply.github.com>
2020-10-28 21:25:26 -0800

    Update combat.cpp
    Added random generator

--- combat.cpp
+++ combat.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <bitset>
 #include <cstdint>
+#include <random>
 #include "roomfill.h"
 
 
 using ComponentType = std::uint8_t;
-const ComponentType MAX_COMPONENTS = 16;
+const ComponentType MAX_COMPONENTS = 8;
 using Stats = std::bitset<MAX_COMPONENTS>;
 
 extern "C" Stats attack(Stats, Stats); // written in assembly!
 
 void combat() {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(0, 7);
+
 	Stats weaponStats;
 	weaponStats[1] = 1;
 
