871bbd0320d69b9a2f553cb801e70484170131bc
timothyakline <44985180+timothyakline@users.noreply.github.com>
2020-12-07 23:00:20 -0900

    added comments; solution file not compiling

--- bitmaskcmp.asm
+++ bitmaskcmp.asm
@@ -4,56 +4,57 @@ bits 64 ; allow 64-bit register names
 
 global critCalc ; makes this visible to linker
 critCalc:
-	mov		rax, rcx
-	and  	rax, rdx
-	ret
+	mov		rax, rcx				;store signature #1
+	and  	rax, rdx				;bitmask w/ signature #2
+	ret								:return resulting mask
 
 global typeCalc ; makes this visible to linker
 typeCalc:	
-	mov		rax, rcx	;store attributeW
-	and  	rax, rdx	;compare attribW w/ enemy.type
+	mov		rax, rcx				;store attributeW
+	and  	rax, rdx				;compare attribW w/ enemy.type
 	cmp		rax, 0b0000
-	jg		Effective
+	jg		Effective				;if a type matches.....
 
-	;Check if an ineffective weapon is being used
-	shl		dx, 1
-	cmp		dx, 0b1000
-	jne		NotEffective
-	shr		dx, 3		;wrap bits in the 12-bit 
+	;Otherwise, check if an ineffective weapon is being used..
+	shl		rdx, 1					;..which differs by a bitshift
+	cmp		rdx, 0b1000
+	jne		NotEffective			;if resulting bitshift is kept in the 3 bit-space...
+	shr		rdx, 3					;otherwise, wrap bits in the 3-bit space
 
-	NotEffective:
-		;mov		rsi, 1		;for print
-		mov		rax, -2		;for attack
-		mov		rdi, rcx
-		and		rdi, rdx
-		cmp		rdi, 0b0000
-		jg		Exit		;ideally, Print
-		
-		mov		rax, rdi
-		jmp		Exit
+	NotEffective:					;...set (dis)advantage to -2
+		   ;mov rsi, 1		;for print
+		mov		rax, -2				;for attack
+		mov		rdi, rcx			;store attributeW again
+		and		rdi, rdx			;compare attribW w/ bitshifted enemy.type
+		cmp		rdi, 0b0000			;check if a type matches
+		jg		Exit				;ideally, jmp to Print
+
+	NoEffect:						;otherwise, set no advantage
+		mov		rax, rdi			;assuming resulting bitmask is 0..
+		jmp		Exit				;return
 	
-	Effective:
-		;mov		rsi, 0		;for print
-		mov		rax, 2		;for attack
+	Effective:						;.....set advantage to +2
+		   ;mov rsi, 0		;for print
+		mov		rax, 2				;for attack,
 	
-	;Print:								;Currently only works on NetRun
-		;push		rax
-		;mov 		rdi,[Feedback+rsi*8]
+		;Print:						;Currently, works great on NetRun
+			;push		rax
+			;mov 		rdi,[Feedback+rsi*8]
 
-		;extern 	puts
-		;call		puts
+			;extern 	puts
+			;call		puts
 
-		;pop		rax
+			;pop		rax
 		
 	Exit:
 		ret
 
 
-		;array of responses
+											;array of responses
 		;section .data
 		;response1: db `SUPER EFFECTIVE\n`,0
 		;response2: db `Not Very Effective.\n`,0
 
 		;Feedback:
-			;dq response1
-			;dq response2
+			;dq response1					;arr[0]
+			;dq response2					;arr[1]
